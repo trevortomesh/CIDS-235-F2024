2c47f27b0b07aec452996d3b1c465bf1967dc222
trevortomesh <tmtomesh@hotmail.com>
2024-09-11 23:13:07 -0500

    update code

---
+++ src/Cat.java
@@ -0,0 +1,9 @@
+public class Cat
+{
+    String name;
+    int age;
+    char sex;
+    double weight;
+    String color;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cat glitch = new Cat();
+        glitch.name = "Glitch McPixel Tomesh";
+        glitch.age = 13;
+        glitch.weight = 15.0;
+        glitch.sex = 'M';
+        glitch.color = "Tuxedo";
+        System.out.println(glitch.name);
+
+        //System.out.println("Hello world!");
+//        String cat1Name = "Glitch";
+//        String cat2Name = "Haku";
+//        String cat3Name = "Genki";
+//        String cat4Name = "Soo soo";
+//        String cat5Name = "Tsuki";
+//        String cat6Name = "Wooper";
+//
+//        int cat1Age = 13;
+//        int cat2Age = 4;
+//        int cat3Age = 1;
+//        int cat4Age = 0;
+//        int cat5Age = 2;
+//        int cat6Age = 1;
+//
+//        char cat1Sex = 'M';
+//        char cat2Sex = 'M';
+//        char cat3Sex = 'M';
+//        char cat4Sex = 'F';
+//        char cat5Sex = 'M';
+//        char cat6Sex = 'M';
+//
+//        double cat1Weight = 15.0;
+//        double cat2Weight = 13.5;
+//        double cat3Weight = 14.0;
+//        double cat4Weight = 5.0;
+//        double cat5Weight = 8.0;
+//        double cat6Weight = 0.2;
+//
+//        String cat1Color = "tuxedo";
+//        String cat2Color = "tuxedo";
+//        String cat3Color = "orange";
+//        String cat4Color = "black";
+//        String cat5Color = "black";
+//        String cat6Color = "pink";
+
+        //sayMeow(cat1Name,cat6Color);
+
+    }
+
+    public static void sayMeow(String cat, String catColor){
+        System.out.println(cat + " a " + catColor + " cat says MEOW!");
+    }
+
+}
